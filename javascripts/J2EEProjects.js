3ff111843195b19cd934db08dc5cafd8cf6f7620
vernkennedy <vzidana14@wooster.edu>
2015-07-21 23:57:50 -0700

    2

--- javascripts/J2EEProjects.js
+++ javascripts/J2EEProjects.js
@@ -14,18 +14,7 @@ var PHPProjectSPortfolio =
       "frontend_infor": "Html5,Html5 Drag and Drop API,CSS3,JavaScript,jQuery",      
       "backend_infor": "Php5,MySQL,MS Excel"    
     },
-    {
-		"id":2,
-        "project_name": "Project Portfolio Tracker",
-      "owner": {       
-        "avatar_url": "https://vzidana.files.wordpress.com/2014/12/340896_10150294808933267_508433853_o-copy-2.jpg", 
-      },           
-      "description": "https://github.com/redguide/nodejs",      
-      "code_url": "https://api.github.com/repos/redguide/nodejs", 
-	 "demo_url": "https://www.youtube.com/watch?v=lJM2oFMoDkM",
-      "frontend_infor": "html",      
-      "backend_infor": "mysql"  
-    },
+   
     {
 		"id":3,
        "project_name": "Client Infor System",
@@ -72,7 +61,7 @@ var PHPProjectSPortfolio =
 text = "";
 var i;
 for (i = 0; i < PHPProjectSPortfolio.PHPProjects.length; i++) {
-    text +="<li><img src="+PHPProjectSPortfolio.PHPProjects[i].owner.avatar_url+"><br> <b>Project Name</b><br>"+ PHPProjectSPortfolio.PHPProjects[i].project_name + "<br><b>Description</b><br><a href="+PHPProjectSPortfolio.PHPProjects[i].description+ ">Description</a><br><b>Video Demo</b><br><a href="+PHPProjectSPortfolio.PHPProjects[i].demo_url+ ">Demo</a><br><b>Code Repository</b><br><a href="+PHPProjectSPortfolio.PHPProjects[i].code_url + ">Code</a><br><b>Front-End Technologies</b><br>"+PHPProjectSPortfolio.PHPProjects[i].frontend_infor + "<br><b> Back-End Technologies</b><br>"+PHPProjectSPortfolio.PHPProjects[i].backend_infor +"</li>";
+    text +="<li><img src="+PHPProjectSPortfolio.PHPProjects[i].owner.avatar_url+"><br><br><h3 align=center>"+ PHPProjectSPortfolio.PHPProjects[i].project_name + "</h3><a href="+PHPProjectSPortfolio.PHPProjects[i].description+ ">Description</a> <a href="+PHPProjectSPortfolio.PHPProjects[i].demo_url+ ">Demo</a> <a href="+PHPProjectSPortfolio.PHPProjects[i].code_url + ">Code</a><br><br><br><br><br></li>";
 }
 
 
