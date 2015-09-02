7587879603e966172b57c153bb7ea0f822fdad9f
vernkennedy <vzidana14@wooster.edu>
2015-09-02 10:18:47 -0700

    41

--- javascripts/J2EEProjects.js
+++ javascripts/J2EEProjects.js
@@ -1,6 +1,6 @@
 var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
-  $http.get("https://raw.githubusercontent.com/vernkennedy/JAVA-J2EEProjects/gh-pages/javascripts/JAVAProjects.json")
+  $http.get("https://raw.githubusercontent.com/vernkennedy/JAVA-J2EEProjects/gh-pages/javascripts/J2EE")
   .success(function (response) {$scope.names = response.records;});
 });
 
