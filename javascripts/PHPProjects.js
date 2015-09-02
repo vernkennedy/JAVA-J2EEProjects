15e2ce07b7b7cf695a164f69c04d6bab64513db9
vernkennedy <vzidana14@wooster.edu>
2015-09-02 10:03:27 -0700

    40

--- javascripts/PHPProjects.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
-  $http.get("https://raw.githubusercontent.com/vernkennedy/PHPProjects/gh-pages/javascripts/PHPProjects.json")
-  .success(function (response) {$scope.names = response.records;});
-});
